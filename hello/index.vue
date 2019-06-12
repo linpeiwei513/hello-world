f6a61cb72fb6df172580923a602d80d8246332de
linpeiwei <linpeiwei@lb-dj.com>
2019-06-12 16:11:35 +0800

    提交测试文件

---
+++ hello/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="hello">
+		<h1>{{title}}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'hello',
+		data () {
+			return {
+				title: '这是一个测试的数据标题',
+			}
+		},
+		//页面加载后是执行
+		created() {
+			
+		}
+	}
+</script>
+
+<style>
+    .hello{
+        height: 100px;
+    }
+</style>
